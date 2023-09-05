9e76c774fbbac4239f9af9c89f22e9cae6631d68
jcblanc2 <jclaytonblanc@gmail.com>
2023-09-05 11:32:23 -0400

    Add local storage

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 let score = JSON.parse(localStorage.getItem("score")) || {
-    wins: 0,
-    loses: 0,
-    ties: 0
+  wins: 0,
+  loses: 0,
+  ties: 0
 };
+let isAutoPlay = false;
+let intervalId;
+let autoPlayBtn = document.querySelector(".auto-play-btn").innerHTML;
+
+// play with keyboard
+document.body.addEventListener("keydown", (event) => {
+  if(event.key === 'r'){
+    playGame("rock");
+  }else if(event.key === 'p'){
+    playGame("paper");
+  }else if(event.key === 's'){
+    playGame("scissors")
+  }
+});
 
 displayScore();
 
@@ -14,88 +28,105 @@ function displayScore(){
 
 // generate number between [min, max]
 function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // reset the score
 function reset() {
-    score.wins = 0;
-    score.loses = 0;
-    score.ties = 0;
-    localStorage.removeItem("score");
-    displayScore();
+  score.wins = 0;
+  score.loses = 0;
+  score.ties = 0;
+  localStorage.removeItem("score");
+  displayScore();
+}
+
+// auto play computer 
+function autoPlay(){
+  if(!isAutoPlay){
+    intervalId = setInterval(() => {
+      const playeMove = getRandom(1, 3) == 1 ? "rock" : getRandom(1, 3) == 2 ? "paper" : "scissors";
+      playGame(playeMove)
+    }, 1000);
+
+    isAutoPlay = true;
+    document.querySelector(".auto-play-btn").innerHTML = "Stop";
+  }else{
+    clearInterval(intervalId);
+    isAutoPlay = false;
+    document.querySelector(".auto-play-btn").innerHTML = "Auto Play";
+  }
 }
 
 // save the score in local storage
 function saveScore(score) {
-    localStorage.setItem("score", score);
+  localStorage.setItem("score", score);
 }
 
 
 function playGame(playeMove){
-    // generate number between [1, 3]	
-    computerChoice = getRandom(1, 3) == 1 ? "rock" : getRandom(1, 3) == 2 ? "paper" : "scissors";
-    let result = ''
+  // generate number between [1, 3]	
+  computerChoice = getRandom(1, 3) == 1 ? "rock" : getRandom(1, 3) == 2 ? "paper" : "scissors";
+  let result = ''
 
-    // check state
-    switch (playeMove) {
-        case "rock":
-          if (computerChoice === "scissors") {
-            result = "You win";
-          } else if (computerChoice === "paper") {
-            result = "You lose";
-          } else {
-            result = "Tie";
-          }
-          break;
-      
-        case "paper":
-          if (computerChoice === "rock") {
-            result = "You win";
-          } else if (computerChoice === "scissors") {
-            result = "You lose";
-          } else {
-            result = "Tie";
-          }
-          break;
-      
-        case "scissors":
-          if (computerChoice === "paper") {
-            result = "You win";
-          } else if (computerChoice === "rock") {
-            result = "You lose";
-          } else {
-            result = "Tie";
-          }
-          break;
-      
-        default:
-          result = "Invalid player move";
-    }
+  // check state
+  switch (playeMove) {
+      case "rock":
+        if (computerChoice === "scissors") {
+          result = "You win";
+        } else if (computerChoice === "paper") {
+          result = "You lose";
+        } else {
+          result = "Tie";
+        }
+        break;
+    
+      case "paper":
+        if (computerChoice === "rock") {
+          result = "You win";
+        } else if (computerChoice === "scissors") {
+          result = "You lose";
+        } else {
+          result = "Tie";
+        }
+        break;
+    
+      case "scissors":
+        if (computerChoice === "paper") {
+          result = "You win";
+        } else if (computerChoice === "rock") {
+          result = "You lose";
+        } else {
+          result = "Tie";
+        }
+        break;
+    
+      default:
+        result = "Invalid player move";
+  }
 
-    if (result === "Tie") {
-        score.ties++;
-    } else if (result === "You lose") {
-        score.loses++;
-    } else {
-        score.wins++;
-    }
+  if (result === "Tie") {
+      score.ties++;
+  } else if (result === "You lose") {
+      score.loses++;
+  } else {
+      score.wins++;
+  }
 
-    // save the score in local storage
-    const jsonString = JSON.stringify(score);
-    saveScore(jsonString);
+  // save the score in local storage
+  const jsonString = JSON.stringify(score);
+  saveScore(jsonString);
 
-    // display message, result and score
-    document.querySelector(".p-result")
-    .innerHTML = result;
+  // display message, result and score
+  document.querySelector(".p-result")
+  .innerHTML = result;
 
-    document.querySelector(".p-moves")
-    .innerHTML = `You
-    <img src="images/${playeMove}-emoji.png" class="move-icon" />
-    <img src="images/${computerChoice}-emoji.png" class="move-icon" />
-    Computer`;
-   
-    displayScore();
+  document.querySelector(".p-moves")
+  .innerHTML = `You
+  <img src="images/${playeMove}-emoji.png" class="move-icon" />
+  <img src="images/${computerChoice}-emoji.png" class="move-icon" />
+  Computer`;
+  
+  displayScore();
 }
